724bdd39d165330103f1a31b2cdd5636bec8d580
Владимир <radlovackivladimir@gmail.com>
2026-06-18 12:10:29 +0200

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/finance-tracker/",
+  base: "/Finance-project/",
   resolve: {
     alias: {
       "@identity": path.resolve(__dirname, "src/identity"),
